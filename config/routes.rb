cebf40130c969ae50589aaff787eec9e06c59e2e
Delta <d@qiscus.com>
2013-08-24 16:15:07 +0700

    completed static pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ NewSampleApp::Application.routes.draw do
 
   get "static_pages/about"
 
+  get "static_pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
